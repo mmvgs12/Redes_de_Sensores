572960fedb828c18d32bc49482adc892af4d79ca
mmvgs12 <sergiomhu@gmail.com>
2026-02-20 13:38:13 +0100

    ultimos commits practica 20/02

--- Practica_2/test.py
+++ Practica_2/test.py
@@ -14,33 +14,44 @@ def PortExists(DesPort='COM1'):
 
     return DesPort in ports_list
 
-def SaveLine (List_name = "Save.txt"):
-    return       
-    #with  open (List_name, 'a') as messages_list:
-                  
+
+def ReadFromSerial(ser, Port = "COM7"):
+    print(PortExists(Port))
+    if PortExists(Port):
+        try:
+            ln = ser.readline().decode("UTF-8").strip().strip("[]").replace(",",";")
+            print(ln)
+            return ln
+        except:
+            print("fallo al leer del serial")
+        
+    else:
+        print("Error, el puerto no existe")
+
+
+def AddtoList(List_name = "Save.txt", line ):
+
+    with open(List_name, 'a') as list:
+        list.write("")
+
+
 
      
 def main():
     print(PortExists("COM7"))
+    try:
+       with serial.Serial(port="COM7", baudrate = 19200) as ser:
+           for i in range (0,100,1):
+                print("lectura numero: " + str(i))
+                ReadFromSerial(ser)
+    except:
+        print("Error, no se ha podido crear el puerto")
 
-    if PortExists("COM7"):
-        try:
-            ser = serial.Serial(port="COM7", baudrate = 19200)
-        except:
-            print("Error, no se ha podido crear el puerto")
+    
+            
+    
 
-        for i in range (0,100,1):
-            ln = ser.readline().strip()
-            ln_dec=ln.decode("UTF-8")
-            Axis = ln_dec.strip("[]").split(", ")
-            print("muestra numero " + str(i))
-            print(Axis[0])
-            print(Axis[1])
-            print(Axis[2])
-
-    else:
-            print("Error, el puerto no existe")
-
+    #AddtoList("Test.txt")
 
 if __name__ == "__main__":
     main()        
