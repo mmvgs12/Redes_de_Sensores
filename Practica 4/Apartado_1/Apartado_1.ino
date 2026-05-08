5715218b1ea1e3f44c7ff86f3b5a8ec5c52197e6
mmvgs12 <sergiomhu@gmail.com>
2026-05-08 09:17:04 +0200

    Update Apartado_1.ino

--- Practica 4/Apartado_1/Apartado_1.ino
+++ Practica 4/Apartado_1/Apartado_1.ino
@@ -1,30 +1,63 @@
 #include <ArduinoBLE.h>
+#include <Arduino_LSM9DS1.h>
 
-const char* MY_UUID    = "19B10000-5365-7267-696F-000000000001"; //UUID para serivicio generico con mi nombre 5365-7267-696F
-const char* ADVERTISING_NAME = "NanoIoT-Sergio_Muñoz"; //Advertising del dispositivo
- 
-BLEService advertService(MY_UUID);
+const char* DEVICE_NAME = "NanoIoT-Accel_S_M";
+
+BLEService imuService("1101");
+// Característica solo lectura + notificaciones: acelerómetro
+BLEStringCharacteristic accelChar("2101", BLERead | BLENotify, 48);
+// Característica solo escritura: activar/desactivar
+BLEStringCharacteristic enableChar("2102", BLEWrite, 4);
+
+bool accelEnabled = true;
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
-
-  if (!BLE.begin()) {
-    Serial.println("ERROR: no se pudo iniciar BLE"); //Si falla la conexion al BLE, "para" el programa
-    while(1);
-  }
-  //Genera el adviertising con los servicios y caracteristicas para poder verlo desde nrfConnect
-  BLE.setLocalName(ADVERTISING_NAME);
-  BLE.setAdvertisedService(advertService);
-  BLE.addService(advertService);
+  IMU.begin();
+  BLE.begin();
+  BLE.setLocalName(DEVICE_NAME);
+  BLE.setAdvertisedService(imuService);
+  imuService.addCharacteristic(accelChar);
+  imuService.addCharacteristic(enableChar);
+  BLE.addService(imuService);
   BLE.advertise();
-
-  Serial.print("Advertising BLE como: ");
-  Serial.println(ADVERTISING_NAME);
 
 }
 
 void loop() {
-  BLE.poll(); // necesario para procesar eventos BLE
-  delay(200);
+  BLEDevice central = BLE.central();
+
+  while (central.connected()) {
+    // Procesar comando enable/disable
+    if (enableChar.written()) {
+      String read = enableChar.value();
+      read.toUpperCase();
+      Serial.print(read);
+
+      if(read == "T")
+      {
+        accelEnabled = true;
+      } else if (read == "F")
+      {
+        accelEnabled = false;
+      }
+    }
+
+    if (accelEnabled && IMU.accelerationAvailable()) {
+      float ax, ay, az;
+      IMU.readAcceleration(ax, ay, az);
+      char buf[48];
+      
+      Serial.println("Los valores del IMU son: ");
+      Serial. println("eje x: " + String(ax));
+      Serial. println("eje y: " + String(ay));
+      Serial. println("eje z: " + String(az));
+      Serial.println("");
+      
+      snprintf(buf, sizeof(buf), "%.4f,%.4f,%.4f", ax, ay, az);
+      accelChar.writeValue(String(buf));
+    }
+    delay(100); // actualizar cada 100ms
+  }
+  Serial.println("Desconectado.");
 }
